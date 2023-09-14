985f8d53fe7566adc890b750aefd0a3919dc35fa
ScotDev <callum.git@outlook.com>
2023-09-14 17:07:31 +0100

    Removed all next/image elements due to lack of paid resources

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -3,17 +3,22 @@ type AvatarProps = {
   subtitle?: string;
   image_URL?: string;
 };
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Avatar({ name, subtitle, image_URL }: AvatarProps) {
   return (
     <div className="flex space-x-4 items-center">
-      <Image
+      {/* <Image
         src={image_URL ?? "/test.png"}
         alt="User profile picture"
         width={36}
         height={36}
         className="rounded-full ring ring-neutral-600 h-[36px]"
+      /> */}
+      <img
+        src={image_URL ?? "/test.png"}
+        alt="User profile picture"
+        className="rounded-full ring ring-neutral-600 h-9 w-9"
       />
       <div className="flex-col hidden md:flex">
         <p className="text-neutral-100">{name ?? "User"}</p>
